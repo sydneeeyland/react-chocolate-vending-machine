6d57c2cc41b8c9a9a391bc3d60c994b05ed52cb8
asd <yeye@gmail.com>
2022-07-06 18:00:20 +0800

    created vendoitems components for listing of available items for vendo machine

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,23 +3,37 @@ import React from 'react'
 
 // @desc Components
 import Coins from '../Components/Coins';
+import VendoItems from '../Components/VendoItems';
 
 function Home() {
-  return (
-    <React.Fragment>
-        <main className='container-fluid mt-3'>
-            <article className='container'>
-                <section className='d-flex justify-content-center align-items-center'>
-                    <h1>Chocolate Vending Machine</h1>
-                </section>
-                <section>
-                    <Coins />
-                </section>
-                
-            </article>
-        </main>
-    </React.Fragment>
-  )
+
+    const items = [
+        { name: 'Caramel', price: '2.50' }, 
+        { name: 'Hazelnut', price: '3.10' },
+        { name: 'Organic Raw', price: '2.00' }
+    ]
+
+    return (
+        <React.Fragment>
+            <main className='container-fluid mt-3'>
+                <article className='container'>
+
+                    <section className='d-flex justify-content-center align-items-center'>
+                        <h1>Chocolate Vending Machine</h1>
+                    </section>
+
+                    <section>
+                        <VendoItems items={items} />
+                    </section>
+
+                    <section>
+                        <Coins />
+                    </section>
+                    
+                </article>
+            </main>
+        </React.Fragment>
+    )
 }
 
 export default Home
